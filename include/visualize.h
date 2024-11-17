356fab157c521ccbdec79cfb07b00511b2a3959b
Johnathan Burchill <j.burchill@ucalgary.ca>
2024-11-17 10:12:39 -0700

    Custom timeseries plots

--- include/visualize.h
+++ include/visualize.h
@@ -2,7 +2,10 @@
 #define VISUALIZE_H
 
 #include "state.h"
+#include <tiigraphics/draw.h>
 
 int visualizeResults(ProcessorState *state);
+void drawFloatTimeSeries(Image *imageBuf, double *times, float *values, int firstInd, int lastInd, int stride, float valueScale, float minValue, float maxValue, int plotX0, int plotY0, int plotWidth, int plotHeight, const char *xLabel, const char *yLabel, int colorIndex, const char *minValueStr, const char *maxValueStr, bool log10Scale, int dotSize, int fontSize, bool axes, int tupleLength, int tupleIndex);
+
 
 #endif // VISUALIZE_H
--- visualize.c
+++ visualize.c
@@ -2,10 +2,12 @@
 #include "state.h"
 #include "errors.h"
 
+#include <tii/utility.h>
 #include <tiigraphics/draw.h>
 #include <tiigraphics/tiigraphics.h>
 #include <tiigraphics/video.h>
 #include <tiigraphics/colors.h>
+#include <tiigraphics/fonts.h>
 
 #include <stdlib.h>
 #include <string.h>
@@ -83,56 +85,22 @@ int visualizeResults(ProcessorState *state)
 
     int plotHeight0 = 45;
     int plotHeight1 = 30;
-    int plotdy = 15;
+    int plotdy = 25;
 
     int plotX0 = 100;
     int plotY0 = 100;
     int plotY1 = plotY0 + plotHeight0 + plotdy;
     int plotY2 = plotY1 + plotHeight0 + plotdy;
     int plotY3 = plotY2 + plotHeight0 + plotdy;
-    int plotY4 = plotY3 + plotHeight1 + plotdy;
-    int plotY5 = plotY4 + plotHeight1 + plotdy;
+    int plotY4 = plotY3 + plotHeight0 + plotdy;
+    int plotY5 = plotY4 + plotHeight0 + plotdy;
 
-    // Latitude
-    param = (float*)dataBuffers[5];
-    index = 0;
-    for (int i = first; i < last; i++) {
-        values[index++] = (double)param[i];
-    }
-    drawTimeSeries(&image, times, values, nValues, plotX0, plotY0, plotWidth, plotHeight0, state->movieT0/1000.0, state->movieT1/1000.0, -90, 90, "", "QDLat", 1, MAX_COLOR_VALUE + 1, "-90", "90", false, dotSize, 12, true);
-
-    // Vixh
-    param = (float*)dataBuffers[1];
-    index = 0;
-    for (int i = 2*first; i < 2*last; i+=2) {
-        values[index++] = (double)param[i];
-    }
-    drawTimeSeries(&image, times, values, nValues, plotX0, plotY1, plotWidth, plotHeight0, state->movieT0/1000.0, state->movieT1/1000.0, -4000, 4000, "", "Vixh", 1, MAX_COLOR_VALUE + 1, "-4000", "4000", false, dotSize, 12, true);
-
-    // Vixv
-    param = (float*)dataBuffers[2];
-    index = 0;
-    for (int i = 2*first; i < 2*last; i+=2) {
-        values[index++] = (double)param[i];
-    }
-    drawTimeSeries(&image, times, values, nValues, plotX0, plotY2, plotWidth, plotHeight0, state->movieT0/1000.0, state->movieT1/1000.0, -4000, 4000, "", "Vixv", 1, MAX_COLOR_VALUE + 1, "-4000", "4000", false, dotSize, 12, true);
-
-    // Viy
-    param = (float*)dataBuffers[1];
-    index = 0;
-    for (int i = 2*first+1; i < 2*last+1; i+=2) {
-        values[index++] = (double)param[i];
-    }
-    drawTimeSeries(&image, times, values, nValues, plotX0, plotY3, plotWidth, plotHeight0, state->movieT0/1000.0, state->movieT1/1000.0, -4000, 4000, "", "Viy", 1, MAX_COLOR_VALUE + 1, "-4000", "4000", false, dotSize, 12, true);
-
-    // Geopotential
-    param = state->geoPotential;
-    index = 0;
-    for (int i = first; i < last; i++) {
-        values[index++] = (double)param[i]/1000.0;
-    }
-    drawTimeSeries(&image, times, values, nValues, plotX0, plotY4, plotWidth, plotHeight0, state->movieT0/1000.0, state->movieT1/1000.0, -200, 200, "", "Geopot.", 1, MAX_COLOR_VALUE + 1, "-200", "200", false, dotSize, 12, true);
-
+    // Plots
+    drawFloatTimeSeries(&image, (double*)dataBuffers[0], (float*)dataBuffers[5], first, last, 1, 1.0, -90.0, 90.0, plotX0, plotY0, plotWidth, plotHeight0, "", "QD Lat", MAX_COLOR_VALUE + 1, "-90", "90", false, dotSize, 12, true, 1, 0);
+    drawFloatTimeSeries(&image, (double*)dataBuffers[0], (float*)dataBuffers[1], first, last, 1, 0.001, -4.0, 4.0, plotX0, plotY1, plotWidth, plotHeight0, "", "Vixh", MAX_COLOR_VALUE + 1, "-4", "4", false, dotSize, 12, true, 2, 0);
+    drawFloatTimeSeries(&image, (double*)dataBuffers[0], (float*)dataBuffers[2], first, last, 1, 0.001, -4.0, 4.0, plotX0, plotY2, plotWidth, plotHeight0, "", "Vixv", MAX_COLOR_VALUE + 1, "-4", "4", false, dotSize, 12, true, 2, 0);
+    drawFloatTimeSeries(&image, (double*)dataBuffers[0], (float*)dataBuffers[1], first, last, 1, 0.001, -4.0, 4.0, plotX0, plotY3, plotWidth, plotHeight0, "", "Viy", MAX_COLOR_VALUE + 1, "-4", "4", false, dotSize, 12, true, 2, 1);
+    drawFloatTimeSeries(&image, (double*)dataBuffers[0], (float*)dataBuffers[2], first, last, 1, 0.001, -4.0, 4.0, plotX0, plotY4, plotWidth, plotHeight0, "", "Viz", MAX_COLOR_VALUE + 1, "-4", "4", false, dotSize, 12, true, 2, 1);
 
     for (int c = 0; c < 0.1 * VIDEO_FPS; c++) {
         generateFrame(&image, frameCounter++);
@@ -157,3 +125,101 @@ cleanup:
     return TIICT_OK;
 }
 
+void drawFloatTimeSeries(Image *imageBuf, double *times, float *values, int firstInd, int lastInd, int stride, float valueScale, float minValue, float maxValue, int plotX0, int plotY0, int plotWidth, int plotHeight, const char *xLabel, const char *yLabel, int colorIndex, const char *minValueStr, const char *maxValueStr, bool log10Scale, int dotSize, int fontSize, bool axes, int tupleLength, int tupleIndex)
+{
+    int x0, y0;
+    int x, y;
+
+    double t0 = times[firstInd];
+    double t1 = times[lastInd];
+    int nValues = lastInd - firstInd + 1;
+    double timeRange = t1 - t0;
+    double tmpVal;
+    char label[255];
+
+    double tickDeltaTSeconds = 0.0;
+
+    int nTicks = 5;
+    char *tickUnit = "s";
+    if (timeRange < 10.0) {
+        tickDeltaTSeconds = 1.0;
+    } else if (timeRange < 30.0) {
+        tickDeltaTSeconds = 5.0;
+    } else if (timeRange < 60.0) {
+        tickDeltaTSeconds = 10.0;
+    } else if (timeRange < 60.0*10.0) {
+        tickDeltaTSeconds = 60.0;
+        tickUnit = "m";
+    } else if (timeRange < 60.0*60.0) {
+        tickDeltaTSeconds = 300.0;
+    } else if (timeRange < 60.0*60.0*12.0) {
+        tickDeltaTSeconds = 3600.0;
+        tickUnit = "h";
+    } else {
+         tickDeltaTSeconds = 3600.0*3.0;
+        tickUnit = "h";
+    }
+
+    if (timeRange > 0 && nValues > 0)
+    {
+        if (axes)
+        {
+            // Abscissa
+            for (int s = 0; s <= timeRange; s+=tickDeltaTSeconds)
+            {
+
+                sprintf(label, "%d", (int)(s / tickDeltaTSeconds));
+                annotate(label, fontSize, plotX0 + (int)(s / timeRange * plotWidth)-6, plotY0, imageBuf);
+            }
+            annotate(xLabel, fontSize, plotX0 + plotWidth/2 - (strlen(xLabel)*(8*fontSize))/24, plotY0+12, imageBuf);
+            // Ordinate
+            annotate(yLabel, fontSize, plotX0 + plotWidth + 5, plotY0 - plotHeight/2 - 6, imageBuf);
+            annotate(minValueStr, fontSize, plotX0 + plotWidth+3, plotY0 - 8, imageBuf);
+            annotate(maxValueStr, fontSize, plotX0 + plotWidth+3, plotY0 - plotHeight - 8, imageBuf);
+            for (int o = plotY0; o >= plotY0 - plotHeight; o--)
+            {
+                setBufferColorIndex(imageBuf, plotX0 + plotWidth+1, o, FOREGROUND_COLOR);
+            }
+        }
+
+        // data
+        for (int i = firstInd; i < lastInd; i+=stride)
+        {
+            x0 = rescaleAsInteger(times[i], t0, t1, plotX0, plotX0 + plotWidth);
+            tmpVal = values[i*tupleLength + tupleIndex]*valueScale;
+            if (log10Scale)
+            {
+                if (tmpVal > 0)
+                    tmpVal = log10(tmpVal);
+                else
+                    tmpVal = -10000.;
+            }
+            y0 = rescaleAsInteger(tmpVal, minValue, maxValue, plotY0, plotY0 - plotHeight);
+            switch (dotSize)
+            {
+                case 2:
+                    setBufferColorIndex(imageBuf, x0, y0, colorIndex);
+                    setBufferColorIndex(imageBuf, x0+1, y0, colorIndex);
+                    setBufferColorIndex(imageBuf, x0+1, y0+1, colorIndex);
+                    setBufferColorIndex(imageBuf, x0, y0+1, colorIndex);
+                    break;
+                case 3:
+                    setBufferColorIndex(imageBuf, x0, y0, colorIndex);
+                    setBufferColorIndex(imageBuf, x0, y0-1, colorIndex);
+                    setBufferColorIndex(imageBuf, x0, y0+1, colorIndex);
+                    setBufferColorIndex(imageBuf, x0+1, y0, colorIndex);
+                    setBufferColorIndex(imageBuf, x0+1, y0-1, colorIndex);
+                    setBufferColorIndex(imageBuf, x0+1, y0+1, colorIndex);
+                    setBufferColorIndex(imageBuf, x0-1, y0, colorIndex);
+                    setBufferColorIndex(imageBuf, x0-1, y0-1, colorIndex);
+                    setBufferColorIndex(imageBuf, x0-1, y0+1, colorIndex);
+                    break;
+                default:
+                    setBufferColorIndex(imageBuf, x0, y0, colorIndex);
+                    break;
+            }
+        }
+    }
+
+    return;
+}
